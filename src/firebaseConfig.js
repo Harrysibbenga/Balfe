c6cb0d5e69731243aee871746c40fec8a9a032c7
Harry's Torque MacBook <harry@Harry.local>
2020-03-25 13:41:29 +0000

    changed config variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,25 @@
 import firebase from "firebase";
 import "firebase/firestore";
-const dotenv = require('dotenv');
-dotenv.config();
+const {
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    measurementId,
+    appId,
+    messagingSenderId
+} = require('./config');
 // firebase init goes here
 const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
 };
 
 // Initialize Firebase
