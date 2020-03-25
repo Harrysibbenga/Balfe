4da4ecf118e2e20569fcbb8ca3606c851c1f45fe
Harry's Torque MacBook <harry@Harry.local>
2020-03-25 14:53:11 +0000

    removed config file and firebase files

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,25 +1,15 @@
 import firebase from "firebase";
 import "firebase/firestore";
-const {
-    apiKey,
-    authDomain,
-    databaseURL,
-    projectId,
-    storageBucket,
-    measurementId,
-    appId,
-    messagingSenderId
-} = require('../config');
+
 // firebase init goes here
 const config = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    databaseURL: databaseURL,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId,
-    measurementId: measurementId
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY || 'api-key-not-set',
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN || 'env-not-set',
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL || 'env-not-set',
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID || 'env-not-set',
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET || 'env-not-set',
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || 'env-not-set',
+    appId: process.env.VUE_APP_FIREBASE_APP_ID || 'env-not-set',
 };
 
 // Initialize Firebase
