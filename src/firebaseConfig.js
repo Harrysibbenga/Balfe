1570c540d721f695fb4de745d88ce46323745097
Harry's Torque MacBook <harry@Harry.local>
2020-03-25 13:30:17 +0000

    changed project id

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
 import "firebase/firestore";
-
+const dotenv = require('dotenv');
+dotenv.config();
 // firebase init goes here
 const config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
-    projectId: "balfemotorsport-9a12d",
+    projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
