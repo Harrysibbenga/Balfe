ea3bded31eca2799d8d75e9170df44a47d0e553a
Harry's Torque MacBook <harry@Harry.local>
2020-03-25 15:12:52 +0000

    run rebuild

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 
 // firebase init goes here
 const config = {
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY || 'api-key-not-set',
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN || 'env-not-set',
-    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL || 'env-not-set',
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID || 'env-not-set',
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET || 'env-not-set',
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || 'env-not-set',
-    appId: process.env.VUE_APP_FIREBASE_APP_ID || 'env-not-set',
-    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID || 'env-not-set'
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
