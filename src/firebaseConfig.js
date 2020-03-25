876307cb19adb68df40677d568434d5e192bac69
Harry's Torque MacBook <harry@Harry.local>
2020-03-25 15:01:36 +0000

    removed config file and firebase files

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -10,6 +10,7 @@ const config = {
     storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET || 'env-not-set',
     messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID || 'env-not-set',
     appId: process.env.VUE_APP_FIREBASE_APP_ID || 'env-not-set',
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID || 'env-not-set'
 };
 
 // Initialize Firebase
