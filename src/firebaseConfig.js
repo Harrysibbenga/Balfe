75608b90e79c25b70d86521b0ecc1b804d30f4fc
Harry's Torque MacBook <harry@Harry.local>
2020-03-25 13:25:07 +0000

    changed project id

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,7 +6,7 @@ const config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
+    projectId: "balfemotorsport-9a12d",
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
