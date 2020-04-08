2102b26e82add92f72e95628bf794a5509f8d807
Harry Sibbenga <sibbengaharry@gmail.com>
2020-04-08 15:24:01 +0100

    updated the store to recive data from the images module

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import admin from './Modules/admin';
 import posts from './Modules/posts';
-// import images from "./Modules/images";
+import images from "./Modules/images";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         admin,
-        posts
-        // images
+        posts,
+        images
     }
 })
