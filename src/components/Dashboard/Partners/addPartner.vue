335952f177809373f7d6dfbe44f385ec4238d069
Harry Sibbenga <sibbengaharry@gmail.com>
2020-04-15 16:05:28 +0100

    added the add image buttons and select image logic to the partners section

--- src/components/Dashboard/Partners/addPartner.vue
+++ src/components/Dashboard/Partners/addPartner.vue
@@ -4,14 +4,16 @@
     <hr class="balfe-line" />
     <b-form @submit.prevent="submitHandler">
       <transition name="fade">
-        <div v-if="performingRequest" class="loading">
+        <div v-if="request" class="loading">
           <p class="text-dark">Loading...</p>
         </div>
       </transition>
-      <img :src="partner.logo" class="img-fluid" />
-      <div class="input_field">
-        <input type="file" @change="uploadImage" ref="fileInput" accept="image/*" />
+
+      <div class="container">
+        <img :src="partner.logo" class="img-fluid" />
       </div>
+
+      <b-button class="btn btn-outline-primary" @click="addNewImage($event)" value="partner">+ Image</b-button>
 
       <b-form-group id="name" label="Name" label-for="name" class="text-left">
         <b-input
@@ -20,7 +22,6 @@
           type="text"
           required
           placeholder="Partner name"
-          v-model="partner.name"
         ></b-input>
       </b-form-group>
 
@@ -28,10 +29,79 @@
         <wysiwyg id="desc" v-model="partner.desc"></wysiwyg>
       </b-form-group>
 
-      <b-button type="submit" :disabled="partner.desc == ''" variant="primary">Add post</b-button>
+      <b-button type="submit" :disabled="partner.desc == ''" variant="primary">Add partner</b-button>
     </b-form>
 
-    <div v-if="partnerAdded" variant="success">Your partner has been added!</div>
+    <div v-if="partnerAdded" class="text-success">Your partner has been added!</div>
+
+    <!-- Modals -->
+    <!-- New image modal  -->
+    <b-modal
+      v-model="newImage"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="xl"
+    >
+      <h5>Upload a new image</h5>
+
+      <transition name="fade">
+        <div v-if="request" class="loading">
+          <p class="text-dark">Loading...</p>
+        </div>
+      </transition>
+
+      <div class="container">
+        <div v-if="logoUrl">
+          <img :src="logoUrl" class="img-fluid" />
+        </div>
+      </div>
+
+      <input
+        type="file"
+        @change="checkFile"
+        ref="fileInput"
+        accept="image/*"
+        v-if="input == 'partner'"
+      />
+
+      <div class="d-block text-right pt-3">
+        <b-button class="mr-2" @click="close">Close</b-button>
+      </div>
+
+      <p class="text-success" v-if="confirmation">Image uploaded !</p>
+    </b-modal>
+    <!-- New image modal  -->
+
+    <!-- User Prompt  -->
+    <b-modal
+      v-model="userPrompt"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="xl"
+    >
+      <h5>Duplicate image</h5>
+
+      <div class="container">
+        <img :src="logoUrl" class="img-fluid" />
+      </div>
+
+      <p>Duplicate image would you like to use this image ?</p>
+
+      <div class="d-block text-right pt-3">
+        <b-button class="mr-2" @click="confirm">Yes</b-button>
+        <b-button class="mr-2" @click="decline">No</b-button>
+      </div>
+    </b-modal>
+    <!-- User Prompt  -->
+    <!-- Modals -->
   </div>
 </template>
 
@@ -47,8 +117,11 @@ export default {
         desc: "",
         logo: ""
       },
-      performingRequest: false,
-      partnerAdded: null
+      logoId: null,
+      partnerAdded: null,
+      userPrompt: null,
+      newImage: null,
+      input: null
     };
   },
   validations: {
@@ -59,58 +132,114 @@ export default {
     }
   },
   methods: {
-    uploadImage(event) {
+    // adding a new image
+    addNewImage(event) {
+      this.newImage = true;
+      if (event.target.value == "partner") {
+        this.input = "partner";
+      }
+    },
+    //check the file to see if it exists
+    checkFile(event) {
       let file = event.target.files[0];
-      let storageRef = fb.storage.ref("logos/" + file.name);
-      let uploadTask = storageRef.put(file);
-      this.performingRequest = true;
-      uploadTask.on(
-        "state_changed",
-        () => {
-          // snapshot
-        },
-        () => {
-          // Handle unsuccessful uploads
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-            this.partner.logo = downloadURL;
-            fb.logosCollection
-              .add({
-                name: file.name,
-                createdOn: new Date(),
-                url: downloadURL
-              })
-              .then(() => {
-                this.performingRequest = false;
-              })
-              .catch(error => alert(error.message));
+      let exist = false;
+      fb.logosCollection
+        .where("name", "==", file.name)
+        .get()
+        .then(docs => {
+          docs.forEach(doc => {
+            if (doc.exists) {
+              exist = true;
+              if (this.$refs.fileInput) {
+                this.partner.logo = doc.data().url;
+                this.logoId = doc.id;
+                this.userPrompt = true;
+                this.$store.commit("images/setLogoUrl", doc.data().url);
+                this.type = "partner";
+              }
+              return;
+            }
           });
-        }
-      );
+        })
+        .then(() => {
+          if (!exist) {
+            this.$store.dispatch("images/uploadLogo", file);
+          }
+        });
+    },
+    // prompt user that image exists
+    decline() {
+      if (this.type == "partner") {
+        this.userPrompt = false;
+        this.$store.commit("images/setLogoUrl", null);
+        this.logoId = null;
+        this.$refs.fileInput.value = "";
+        this.partner.logo = null;
+        this.type == "";
+      }
+    },
+    confirm() {
+      if (this.type == "partner") {
+        this.partner.logo = this.$store.getters["images/getLogoUrl"];
+        this.userPrompt = false;
+        this.$refs.fileInput.value = "";
+        this.type = "";
+        this.newImage = false;
+        this.$store.commit("images/setLogoUrl", null);
+      }
+    },
+    close() {
+      if (this.input == "partner") {
+        let url = this.$store.getters["images/getLogoUrl"];
+        this.partner.logo = url;
+        this.newImage = false;
+        fb.logosCollection
+          .where("url", "==", url)
+          .get()
+          .then(docs => {
+            docs.forEach(doc => {
+              this.logoId = doc.id;
+            });
+          });
+        this.$refs.fileInput.value = "";
+        this.$store.commit("images/setLogoUrl", null);
+        this.type = "";
+      }
     },
     submitHandler() {
+      this.$store.commit("images/setRequest", true);
       fb.partnersCollection
         .add({
           name: this.partner.name,
           desc: this.partner.desc,
-          logo: this.partner.logo,
+          logoUrl: this.partner.logo,
+          logoId: this.logoId,
           createdOn: new Date()
         })
         .then(() => {
+          this.partner.name = "";
+          this.partner.desc = "";
+          this.partner.logo = "";
+          this.logoId = "";
+          this.$v.$reset();
           this.partnerAdded = true;
-        })
-        .then(() => {
-          (this.$refs.fileInput.value = ""),
-            (this.partner.name = ""),
-            (this.partner.desc = ""),
-            (this.partner.logo = "");
+          this.$store.commit("images/setRequest", false);
+          this.$store.commit("images/setLogoUrl", null);
           setTimeout(() => {
-            this.partnerAdded = null;
+            this.partnerAdded = false;
           }, 3000);
         });
+    }
+  },
+  computed: {
+    request() {
+      return this.$store.getters["images/getRequest"];
+    },
+    confirmation() {
+      return this.$store.getters["images/confirmation"];
+    },
+    logoUrl() {
+      return this.$store.getters["images/getLogoUrl"];
     }
   }
 };
--- src/components/Dashboard/Partners/listPartners.vue
+++ src/components/Dashboard/Partners/listPartners.vue
@@ -17,7 +17,7 @@
           <tr v-for="(partner, index) in partners" :key="index">
             <th scope="row">{{ partner.name }}</th>
             <td>
-              <img :src="partner.logo" class="img-fluid" />
+              <img :src="partner.logoUrl" class="img-fluid" width="200px" />
             </td>
             <td>{{ partner.desc }}</td>
             <td>{{ partner.createdOn | formatCreation }}</td>
@@ -30,7 +30,58 @@
       </table>
     </div>
     <div v-else>Sorry no partners yet .......... Add some!</div>
-    <b-modal v-model="deleteDialog" hide-footer hide-header centered>
+    <!-- Models -->
+    <!-- New image modal  -->
+    <b-modal
+      v-model="newImageUpload"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="xl"
+    >
+      <h5>Upload a new image</h5>
+
+      <transition name="fade">
+        <div v-if="request" class="loading">
+          <p class="text-dark">Loading...</p>
+        </div>
+      </transition>
+
+      <div class="container">
+        <div v-if="logoUrl">
+          <img :src="logoUrl" class="img-fluid" />
+        </div>
+      </div>
+
+      <input
+        type="file"
+        @change="checkFile"
+        ref="fileInput"
+        accept="image/*"
+        v-if="input == 'partner'"
+      />
+
+      <div class="d-block text-right pt-3">
+        <b-button class="mr-2" @click="close">Close</b-button>
+      </div>
+
+      <p class="text-success" v-if="confirmation">Image uploaded !</p>
+    </b-modal>
+    <!-- New image modal  -->
+    <!-- Delete  -->
+    <b-modal
+      v-model="deleteDialog"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="lg"
+    >
       <div class="d-block text-center">
         <p>
           Are you sure you want to delete this partner ? ( there's no turning
@@ -42,35 +93,41 @@
         <b-button variant="primary" @click="deleteOnCancel">No, do not delete it</b-button>
       </div>
     </b-modal>
-
-    <b-modal v-model="editDialog" hide-footer hide-header centered>
+    <!-- Delete  -->
+    <!-- Edit -->
+    <b-modal
+      v-model="editDialog"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="xl"
+    >
       <transition name="fade">
-        <div v-if="performingRequest" class="loading">
+        <div v-if="request" class="loading">
           <p class="text-dark">Loading...</p>
         </div>
       </transition>
       <b-form @submit.prevent>
-        <div v-if="selectImage.selected">
-          <img :src="selectImage.selectedUrl" class="mt-3 img-fluid" />
+        <div>
+          <img :src="currentPartner.logoUrl" v-if="!newImage" class="img-fluid" />
+          <img :src="partner.logoUrl" v-if="newImage" class="img-fluid" />
         </div>
-        <div v-else>
-          <img :src="currentPartner.logo" v-if="!newImage" class="img-fluid" />
-          <img :src="partner.logo" v-if="newImage" class="img-fluid" />
-        </div>
-        <div class="input_field">
-          <input type="file" @change="uploadImage" ref="fileInput" accept="image/*" />
-        </div>
-        <b-form-select
-          v-model="selectImage.selected"
-          :options="partnerOptions"
-          size="sm"
-          class="mt-3"
-          @change="selectOption"
-        >
-          <template v-slot:first>
-            <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
-          </template>
-        </b-form-select>
+
+        <b-button
+          class="btn btn-outline-primary"
+          @click="addNewImage($event)"
+          value="partner"
+        >+ Image</b-button>
+
+        <b-button
+          class="btn btn-outline-primary ml-2"
+          @click="chooseImage($event)"
+          value="partner"
+        >+ Select Image</b-button>
+
         <b-form-group id="name" label="name" label-for="name" class="text-left">
           <b-input
             id="name"
@@ -93,16 +150,70 @@
           </wysiwyg>
         </b-form-group>
       </b-form>
+
       <div class="d-block text-right pt-3">
         <b-button class="mr-2" @click="editOnConfirm">Edit</b-button>
         <b-button variant="primary" @click="editOnCancel">Cancel</b-button>
       </div>
     </b-modal>
+    <!-- Edit -->
+    <!-- User Prompt  -->
+    <b-modal
+      v-model="userPrompt"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="xl"
+    >
+      <h5>Duplicate image</h5>
+
+      <div class="container">
+        <img :src="logoUrl" class="img-fluid" />
+      </div>
+
+      <p>Duplicate image would you like to use this image ?</p>
+
+      <div class="d-block text-right pt-3">
+        <b-button class="mr-2" @click="confirm">Yes</b-button>
+        <b-button class="mr-2" @click="decline">No</b-button>
+      </div>
+    </b-modal>
+    <!-- User Prompt  -->
+    <!-- Select single image -->
+    <b-modal
+      v-model="logoChoice"
+      hide-header
+      hide-footer
+      centered
+      no-close-on-backdrop
+      hide-header-close
+      no-close-on-esc
+      size="xl"
+    >
+      <b-form-group label="Logos">
+        <b-form-radio-group id="radio" v-model="select" name="Logos">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-4" v-for="(logo, index) in logos" :key="index">
+                <img class="img-fluid" :src="logo.url" />
+                <b-form-radio :value="logo.id" class="mx-auto"></b-form-radio>
+              </div>
+            </div>
+          </div>
+        </b-form-radio-group>
+      </b-form-group>
+      <b-button class="mr-2 text-left" @click="confirmSelection">Confirm</b-button>
+      <b-button class="mr-2 text-left" @click="closeSel">Close</b-button>
+    </b-modal>
+    <!-- Select single image  -->
+    <!-- Models  -->
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
 import moment from "moment";
 const fb = require("../../../firebaseConfig");
 
@@ -112,23 +223,37 @@ export default {
       partner: {
         name: "",
         desc: "",
-        logo: ""
+        logoUrl: ""
       },
-      performingRequest: false,
       newImage: false,
+      newImageUpload: false,
+      input: null,
+      select: null,
+      userPrompt: null,
+      logoChoice: null,
       deleteDialog: false,
       editDialog: false,
       currentPartner: {},
       toDelete: null,
-      toEdit: null,
-      selectImage: {
-        selected: null,
-        selectedUrl: null
-      }
+      toEdit: null
     };
   },
   computed: {
-    ...mapState(["partners", "partnerOptions"])
+    partners() {
+      return this.$store.getters["partners/getPartners"];
+    },
+    request() {
+      return this.$store.getters["images/getRequest"];
+    },
+    logoUrl() {
+      return this.$store.getters["images/getLogoUrl"];
+    },
+    confirmation() {
+      return this.$store.getters["images/confirmation"];
+    },
+    logos() {
+      return this.$store.getters["images/getLogos"];
+    }
   },
   methods: {
     deletePartner(partner) {
@@ -157,69 +282,134 @@ export default {
     editOnConfirm() {
       this.editDialog = false;
 
-      let logo = this.partner.logo,
+      let logoUrl = this.partner.logoUrl,
         name = this.partner.name,
-        desc = this.partner.desc;
+        desc = this.partner.desc,
+        logoId = this.logoId;
 
       fb.partnersCollection
         .doc(this.toEdit.id)
         .update({
           createdOn: new Date(),
-          logo: logo,
+          logoUrl: logoUrl,
           name: name,
-          desc: desc
+          desc: desc,
+          logoId: logoId
         })
         .then(() => {
           this.currentPartner = {};
           this.newImage = false;
+          this.logoId = "";
+          this.partner.logoUrl = "";
+          this.partner.name = "";
+          this.partner.desc = "";
         })
         .catch(error => alert(error.message));
     },
-    uploadImage(event) {
-      let file = event.target.files[0];
-      let storageRef = fb.storage.ref("logos/" + file.name);
-      let uploadTask = storageRef.put(file);
-      this.newImage = true;
-      this.selectImage.selected = false;
-      this.performingRequest = true;
-      uploadTask.on(
-        "state_changed",
-        () => {
-          // snapshot
-        },
-        () => {
-          // Handle unsuccessful uploads
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-            this.partner.logo = downloadURL;
-            fb.logosCollection
-              .add({
-                name: file.name,
-                createdOn: new Date(),
-                url: downloadURL
-              })
-              .then(() => {
-                this.performingRequest = false;
-              })
-              .catch(error => alert(error.message));
-          });
-        }
-      );
+    // adding a new image
+    addNewImage(event) {
+      if (event.target.value == "partner") {
+        this.newImageUpload = true;
+        this.input = "partner";
+      }
     },
-    selectOption(id) {
-      if (id) {
+    //check the file to see if it exists
+    checkFile(event) {
+      let file = event.target.files[0];
+      let exist = false;
+      fb.logosCollection
+        .where("name", "==", file.name)
+        .get()
+        .then(docs => {
+          docs.forEach(doc => {
+            if (doc.exists) {
+              exist = true;
+              if (this.input == "partner") {
+                this.partner.logoUrl = doc.data().url;
+                this.newImage = true;
+                this.logoId = doc.id;
+                this.userPrompt = true;
+                this.$store.commit("images/setLogoUrl", doc.data().url);
+              }
+              return;
+            }
+          });
+        })
+        .then(() => {
+          if (!exist) {
+            this.$store.dispatch("images/uploadLogo", file);
+          }
+        });
+    },
+    // prompt user that image exists
+    decline() {
+      if (this.input == "partner") {
+        this.userPrompt = false;
+        this.$store.commit("images/setImageUrl", null);
+        this.logoId = null;
+        this.$refs.fileInput.value = "";
+        this.partner.logoUrl = null;
+        this.input == "";
+        this.newImage = false;
+      }
+    },
+    confirm() {
+      if (this.input == "partner") {
+        this.partner.logoUrl = this.$store.getters["images/getLogoUrl"];
+        this.userPrompt = false;
+        this.$refs.fileInput.value = "";
+        this.newImageUpload = false;
+        this.$store.commit("images/setLogoUrl", null);
+        this.newImage = true;
+        this.input = "";
+      }
+    },
+    close() {
+      if (this.input == "partner") {
+        let url = this.$store.getters["images/getLogoUrl"];
+        this.partner.logoUrl = url;
+        this.newImageUpload = false;
         fb.logosCollection
-          .doc(id)
+          .where("url", "==", url)
+          .get()
+          .then(docs => {
+            docs.forEach(doc => {
+              this.logoId = doc.id;
+            });
+          });
+        this.$refs.fileInput.value = "";
+        this.$store.commit("images/setLogoUrl", null);
+        this.input = "";
+      }
+    },
+    // select image
+    chooseImage(event) {
+      if (event.target.value == "partner") {
+        this.logoChoice = true;
+        this.select = this.currentPartner.logoId;
+      }
+    },
+    confirmSelection() {
+      if (this.logoChoice) {
+        fb.logosCollection
+          .doc(this.select)
           .get()
           .then(doc => {
             let image = doc.data();
-            this.selectImage.selectedUrl = image.url;
-            this.partner.logo = image.url;
+            this.newImage = true;
+            this.partner.logoUrl = image.url;
+            this.logoId = doc.id;
           })
-          .catch(error => alert(error.message));
+          .then(() => {
+            this.logoChoice = false;
+            this.select = null;
+          });
+      }
+    },
+    closeSel() {
+      if (this.logoChoice) {
+        this.logoChoice = false;
+        this.select = null;
       }
     }
   },
@@ -238,6 +428,10 @@ export default {
       let date = val.toDate();
       return moment(date).format("MMMM Do YYYY");
     }
+  },
+  created() {
+    this.$store.dispatch("partners/setPartners");
+    this.$store.dispatch("images/getLogos");
   }
 };
 </script>
