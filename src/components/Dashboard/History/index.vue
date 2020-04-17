b8a0b6bcb762c06ac4cda93e9e1ec971697db55a
Harry Sibbenga <sibbengaharry@gmail.com>
2020-04-17 14:45:10 +0100

    added the logic for adding the historic index and viewing the historic posts stored database

---
+++ src/components/Dashboard/History/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-left">History</h1>
+    <hr class="balfe-line" />
+    <b-nav tabs>
+      <b-nav-item to="/dashboard/history/add_history">Add history</b-nav-item>
+      <b-nav-item to="/dashboard/history/list_history">Manage history posts</b-nav-item>
+    </b-nav>
+    <router-view></router-view>
+  </div>
+</template>
