0d3403fb9ae12f3f1edf9b6391e49e21f572003f
Harry Sibbenga <sibbengaharry@gmail.com>
2020-04-20 16:30:50 +0100

    added code to view the gallery information and pagination

--- src/components/Home/Media.vue
+++ src/components/Home/Media.vue
@@ -1,33 +1,54 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div
-        class="col-12 col-sm-6 col-md-4 col-lg-3 p-0"
-        v-for="(content, index) in galleryContent"
-        :key="index"
-      >
-        <img v-if="content.imageId" :src="content.url" class="img-fluid" />
-        <b-embed
-          type="iframe"
-          aspect="16by9"
-          :src="content.url"
-          allowfullscreen
-          v-else
-        ></b-embed>
+  <div>
+    <h2 class="pl-3 pb-5 font-weight-bold">Media</h2>
+    <div class="container-fluid border-top border-bottom border-primary">
+      <div class="row">
+        <div
+          class="col-12 col-sm-6 col-md-4 col-lg-3 p-0"
+          v-for="(content, index) in galleryContent"
+          :key="index"
+        >
+          <div v-if="content.imageId">
+            <img :src="content.url" class="img-fluid" />
+          </div>
+          <div v-if="content.videoId">
+            <b-embed type="iframe" aspect="16by9" :src="content.url" allowfullscreen></b-embed>
+          </div>
+        </div>
       </div>
     </div>
+
+    <nav aria-label="Media results">
+      <ul class="pagination justify-content-center pt-5">
+        <li class="page-item disabled">
+          <span class="page-link">Previous</span>
+        </li>
+        <li class="page-item">
+          <a class="page-link active" href="#">1</a>
+        </li>
+        <li class="page-item" aria-current="page">
+          <a href="#" class="page-link">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-
 export default {
-  data() {
-    return {};
-  },
   computed: {
-    ...mapState(["galleryContent"])
+    galleryContent() {
+      return this.$store.getters["gallery/getGallery"];
+    }
+  },
+  created() {
+    this.$store.dispatch("gallery/setGallery");
   }
 };
 </script>
