3149bddb04c0428bca96ce1a3224837acb922783
Harry Sibbenga <sibbengaharry@gmail.com>
2020-04-20 16:21:50 +0100

    created the gallery modual

---
+++ src/store/Modules/gallery.js
@@ -0,0 +1,40 @@
+const fb = require("../../firebaseConfig")
+
+const gallery = {
+    namespaced: true,
+    state: {
+        galleryContent: []
+    },
+    mutations: {
+        setGallery(state, val) {
+            if (val) {
+                state.galleryContent = val
+            } else {
+                state.galleryContent = []
+            }
+        }
+    },
+    actions: {
+        setGallery({
+            commit
+        }) {
+            fb.galleryCollection.orderBy("createdOn", "desc").onSnapshot(querySnapshot => {
+                let galleryArray = [];
+
+                querySnapshot.forEach(doc => {
+                    let content = doc.data();
+                    content.id = doc.id;
+                    galleryArray.push(content);
+                });
+                commit("setGallery", galleryArray);
+            });
+        }
+    },
+    getters: {
+        getGallery(state) {
+            return state.galleryContent
+        }
+    }
+}
+
+export default gallery
