3faa95dee8f74ef697ec9ec15194ac48516294a0
Harry Sibbenga <sibbengaharry@gmail.com>
2020-04-22 17:07:29 +0100

    slight changes to href

--- src/components/Header-Foooter/footer.vue
+++ src/components/Header-Foooter/footer.vue
@@ -3,14 +3,12 @@
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">
       © 2020 Copyright:
-      <a class="text-grey" href="https://www.torquetogether.com"
-        >Torque Motorsport</a
-      >
+      <a class="text-grey" href="https://www.torquetogether.com">Torque Motorsport</a>
     </div>
     <!-- Copyright -->
     <!-- button to top -->
     <div class="text-center">
-      <a href="#home" class="btn btn-primary">Back To Top</a>
+      <a href="#" class="btn btn-primary">Back To Top</a>
     </div>
     <!-- button to top -->
   </div>
--- src/components/Header-Foooter/header.vue
+++ src/components/Header-Foooter/header.vue
@@ -10,14 +10,15 @@
       <!-- Right aligned nav items -->
       <b-navbar-nav class="mx-auto align-items-center">
         <!-- Links -->
-        <b-nav-item href="#">Media</b-nav-item>
         <b-nav-item href="#">News</b-nav-item>
+        <b-nav-item href="#media-sec">Media</b-nav-item>
+        <b-nav-item href="#drivers-sec">Drivers</b-nav-item>
+        <b-nav-item href="#calender">Calender</b-nav-item>
+        <b-nav-item href="#partners-sec">Partners</b-nav-item>
         <b-nav-item href="#">History</b-nav-item>
-        <b-nav-item href="#">Drivers</b-nav-item>
-        <b-nav-item href="#">Partners</b-nav-item>
         <b-nav-item v-if="!currentUser" to="/login">Login</b-nav-item>
         <b-nav-item v-if="currentUser" @click="logout">Logout</b-nav-item>
-        <b-nav-item to="dashboard" v-if="currentUser">Dashboard</b-nav-item>
+        <b-nav-item to="/dashboard" v-if="currentUser">Dashboard</b-nav-item>
         <!-- Links -->
       </b-navbar-nav>
     </b-collapse>
